15c7f5bf3ea4283f53474deab773b0f954042896
lrb924 <lara.barger@gmail.com>
2022-08-30 22:48:10 -0500

    Updated fraud_detection_queries.sql

--- fraud_detection_tables.sql
+++ fraud_detection_tables.sql
@@ -1,11 +1,5 @@
-drop table if exists card_holder;
-drop table if exists credit_card;
-drop table if exists merchant_category;
-drop table if exists merchant;
-drop table if exists transaction;
-
 CREATE TABLE card_holder (
-	id INT NOT null,
+	id INT NOT NULL.
 	name VARCHAR(30),
 	PRIMARY KEY (id)
 );
@@ -13,7 +7,7 @@ CREATE TABLE card_holder (
 CREATE TABLE credit_card (
 	card VARCHAR(20) NOT NULL,
 	cardholder_id INT,
-	foreign key (cardholder_id) references card_holder (id),
+	foreign key cardholder_id references card_holder (id),
 	PRIMARY KEY (card)
 );
 
@@ -27,7 +21,7 @@ CREATE TABLE merchant (
 	id INT not NULL,
 	name VARCHAR(30),
 	id_merchant_category INT,
-	foreign key (id_merchant_category) references merchant_category (id),
+	foreign key id_merchant_category references merchant_category (id),
 	primary key (id)
 );
 
@@ -36,9 +30,8 @@ CREATE TABLE transaction (
 	date TIMESTAMP,
 	amount FLOAT,
 	card VARCHAR(20),
-	id_merchant INT,
-	foreign key (card) references credit_card (card),
-	foreign key (id_merchant) references merchant (id),
+	id_merchant INT
+	foreign key card references credit_card (card),
+	foreign key id_merchant references merchant (id),
 	primary key (id)
 );
-
